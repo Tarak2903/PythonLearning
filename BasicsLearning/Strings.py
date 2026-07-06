b9ce0090ddaf618b7e063e8393a0f0a458a7da53
Tarak2903 <tarakkhurana29@gmail.com>
2026-07-06 11:29:27 +0530

    String Learning done

---
+++ BasicsLearning/Strings.py
@@ -0,0 +1,16 @@
+
+name="Tarak"
+greeting=f"Hello {name}"
+
+tarak_greeting="Hello what is your name {jk}"
+print(tarak_greeting.format(jk="Vansh"))
+
+print(greeting)
+
+
+
+'''
+This can be used both for multi line commments as well as string 
+Can be used with f String as well
+
+'''
