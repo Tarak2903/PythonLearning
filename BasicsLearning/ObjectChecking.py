c9e3d3bcaddcdb21c2cebc0a860d794f39d6b89f
Tarak2903 <tarakkhurana29@gmail.com>
2026-07-07 14:50:26 +0530

    TheoryLearning

---
+++ BasicsLearning/ObjectChecking.py
@@ -0,0 +1,23 @@
+'''
+All python objects have three Atrributes
+
+    1.Value  -> a=42  here 42 is the value
+
+    2.Identity -> Its physical Address in the heap Memory
+
+    3.Type -> The class its subclass of for example int
+
+
+
+Numbers are Immutable in python!!!!!!
+
+'''
+
+
+
+
+a=7
+print(id(a))
+
+a=1
+print(id(a))
