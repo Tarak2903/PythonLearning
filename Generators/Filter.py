ca8d42113d951f0931d685508097d4e9187bcaac
Tarak2903 <tarakkhurana29@gmail.com>
2026-07-13 14:20:07 +0530

    Filter and Map done

---
+++ Generators/Filter.py
@@ -0,0 +1,24 @@
+
+ls=["Rohan","Rahul","Tarak"]
+
+
+best_friend= filter(lambda x: x.startswith('R'),ls)
+
+print(next(best_friend))
+print(next(best_friend))
+
+
+
+best_friend=map(lambda r: r.lower(),ls)
+
+print(next(best_friend))
+
+
+
+age=20
+
+def add(current_age):
+    current_age+=1
+
+
+
